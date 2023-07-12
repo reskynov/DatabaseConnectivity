020beecedc31155cf55cac292d4d7a31ffede87a
reskynov <reskykiky2000@gmail.com>
2023-07-12 10:01:03 +0700

    Add Countries

---
+++ Countries.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+
+namespace DatabaseConnectivity
+{
+    public class Countries
+    {
+        private static SqlConnection _connection = DatabaseConnection.Connection();
+
+        public static void GetCountries()
+        {
+            try
+            {
+                _connection.Open();
+
+                SqlCommand cmd = _connection.CreateCommand();
+                cmd.Connection = _connection;
+                cmd.CommandText = "select * from countries";
+                using SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.Write("ID : " + reader.GetString(0));
+                    Console.Write(", Name : " + reader.GetString(1));
+                    Console.WriteLine(", Region ID : " + reader.GetInt32(2));
+                }
+                reader.Close();
+                _connection.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Error connection to database");
+            }
+        }
+
+        //Insert Countries
+        public static void SetCountries(string inputId, string inputCountries, int inputRegionId)
+        {
+            _connection.Open();
+            SqlCommand cmd = _connection.CreateCommand();
+            cmd.Connection = _connection;
+            cmd.CommandText = "insert into countries (id, name, region_id) values (@id, @name, @region_id);";
+
+            SqlTransaction transaction = _connection.BeginTransaction();
+            cmd.Transaction = transaction;
+            try
+            {
+                SqlParameter pId = new SqlParameter();
+                pId.ParameterName = "@id";
+                pId.SqlDbType = System.Data.SqlDbType.Char;
+                pId.Value = inputId;
+                cmd.Parameters.Add(pId);
+
+                SqlParameter pName = new SqlParameter();
+                pName.ParameterName = "@name";
+                pName.SqlDbType = System.Data.SqlDbType.VarChar;
+                pName.Value = inputCountries;
+                cmd.Parameters.Add(pName);
+
+                SqlParameter pRegionId = new SqlParameter();
+                pRegionId.ParameterName = "@region_id";
+                pRegionId.SqlDbType = System.Data.SqlDbType.Int;
+                pRegionId.Value = inputRegionId;
+                cmd.Parameters.Add(pRegionId);
+
+                int result = cmd.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    Console.WriteLine("Insert Success");
+                    transaction.Commit();
+                }
+                else
+                {
+                    Console.WriteLine("Insert Failed");
+                }
+                _connection.Close();
+            }
+            catch
+            {
+                //mengembalikan ke status awal sebelum diinsert
+                transaction.Rollback();
+                Console.WriteLine("Error connection to database");
+            }
+        }
+
+        //Update Region
+        public static void UpdateCountries(string inputCountries, int idRegion)
+        {
+            _connection.Open();
+            SqlCommand cmd = _connection.CreateCommand();
+            cmd.Connection = _connection;
+            cmd.CommandText = "update regions set name = @name where id = @id;";
+
+            SqlTransaction transaction = _connection.BeginTransaction();
+            cmd.Transaction = transaction;
+            try
+            {
+                SqlParameter pName = new SqlParameter();
+                pName.ParameterName = "@name";
+                pName.SqlDbType = System.Data.SqlDbType.VarChar;
+                pName.Value = inputRegion;
+                cmd.Parameters.Add(pName);
+
+                SqlParameter pId = new SqlParameter();
+                pId.ParameterName = "@id";
+                pId.SqlDbType = System.Data.SqlDbType.Int;
+                pId.Value = idRegion;
+                cmd.Parameters.Add(pId);
+
+                int result = cmd.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    Console.WriteLine("Update Success");
+                    transaction.Commit();
+                }
+                else
+                {
+                    Console.WriteLine("Update Failed");
+                }
+                _connection.Close();
+            }
+            catch
+            {
+                //mengembalikan ke status awal sebelum diinsert
+                transaction.Rollback();
+                Console.WriteLine("Error connection to database");
+            }
+        }
+
+        //Delete Region
+        public static void DeleteRegions(int idRegion)
+        {
+            _connection.Open();
+            SqlCommand cmd = _connection.CreateCommand();
+            cmd.Connection = _connection;
+            cmd.CommandText = "delete from regions where id = @id;";
+
+            SqlTransaction transaction = _connection.BeginTransaction();
+            cmd.Transaction = transaction;
+            try
+            {
+                SqlParameter pId = new SqlParameter();
+                pId.ParameterName = "@id";
+                pId.SqlDbType = System.Data.SqlDbType.Int;
+                pId.Value = idRegion;
+                cmd.Parameters.Add(pId);
+
+                int result = cmd.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    Console.WriteLine("Delete Success");
+                    transaction.Commit();
+                }
+                else
+                {
+                    Console.WriteLine("Delete Failed");
+                }
+                _connection.Close();
+            }
+            catch
+            {
+                //mengembalikan ke status awal sebelum diinsert
+                transaction.Rollback();
+                Console.WriteLine("Error connection to database");
+            }
+        }
+
+        //Get By ID Region
+        public static void GetByIdRegions(int idRegion)
+        {
+            try
+            {
+                _connection.Open();
+
+                SqlCommand cmd = _connection.CreateCommand();
+                cmd.Connection = _connection;
+                cmd.CommandText = "select * from regions where id = @id";
+
+                SqlParameter pId = new SqlParameter();
+                pId.ParameterName = "@id";
+                pId.SqlDbType = System.Data.SqlDbType.Int;
+                pId.Value = idRegion;
+                cmd.Parameters.Add(pId);
+
+                using SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.Write("ID : " + reader.GetInt32(0) + " FOUND");
+                    Console.WriteLine(" - Region Name : " + reader.GetString(1));
+                }
+                reader.Close();
+                _connection.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Error connection to database");
+            }
+        }
+    }
+}
+
